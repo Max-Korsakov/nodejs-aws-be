9f038861c0325d720bf87bc2c9ecaae88ff2ebe2
Max Korsakov <Maksim_Karsakou1epam.com>
2020-12-09 22:02:18 +0300

    add product caching

--- bff-service/app.ts
+++ bff-service/app.ts
@@ -2,7 +2,16 @@ import express from 'express';
 
 import axios from 'axios'
 
+const CACHE_EXPIRING_TIME_IN_MS = 120000
+
+const cachedProducts = {
+    productList: null,
+    timeStamp: 0
+}
+
 const application = () => {
+
+
 const app: express.Application = express();
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
@@ -18,17 +27,25 @@ if(recepientURL){
         ...(Object.keys(req.body ||{}).length>0 && {data: req.body})
     }
 axios.defaults
-axios(axiosConfig).then((responce)=>{
-    res.json(responce.data)
-}).catch(error=>{
-    console.log('error', JSON.stringify(error))
-    if(error.responce){
-        const {status, data} = error.responce
-        res.status(status).json(data)
-    } else {
-        res.status(500).json({error: error.message})
-    }
-})
+// Simple caching
+if(Date.now() - cachedProducts.timeStamp < CACHE_EXPIRING_TIME_IN_MS && recepient === 'product'){
+    res.json(cachedProducts.productList)
+} else {
+    axios(axiosConfig).then((responce)=>{
+        cachedProducts.productList = responce.data
+        cachedProducts.timeStamp = Date.now()
+        res.json(responce.data)
+    }).catch(error=>{
+        console.log('error', JSON.stringify(error))
+        if(error.responce){
+            const {status, data} = error.responce
+            res.status(status).json(data)
+        } else {
+            res.status(500).json({error: error.message})
+        }
+    })
+}
+
 } else {
     res.status(502).json({error: 'Cannot process request'})
 }
